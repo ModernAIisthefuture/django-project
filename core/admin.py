bab6b2967f49de7e429a4f5eb2714f703ee713b3
unknown <piyushparmarmca@gmail.com>
2025-09-01 23:24:43 +0530

    Initial commit 1

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Student, Profile, Course, Department
+
+admin.site.register(Student)
+admin.site.register(Profile)
+admin.site.register(Course)
+admin.site.register(Department)
+
